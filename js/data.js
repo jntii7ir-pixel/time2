985e51249443bca541d3dc9596ec7fe34b99752b
jntii7ir-pixel <jntii7ir@gmail.com>
2026-01-20 14:14:58 +0900

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,8 +1,15 @@
 // js/data.js
 
-// とりあえず空の時間割（後で本番データを入れる）
 const timetable = {
-  mon: [],
+  mon: [
+    { name: "1時間目", start: "08:55", end: "09:40" },
+    { name: "2時間目", start: "09:50", end: "10:35" },
+    { name: "3時間目", start: "10:45", end: "11:30" },
+    { name: "4時間目", start: "11:40", end: "12:25" },
+    { name: "5時間目", start: "13:10", end: "13:55" },
+    { name: "6時間目", start: "14:05", end: "14:50" },
+    { name: "7時間目", start: "15:00", end: "15:45" }
+  ],
   tue: [],
   wed: [],
   thu: [],
